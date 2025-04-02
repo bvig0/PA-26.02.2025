27cd6c70c11c3ceceb5a9dbcc8a55d06e0879edb
Brian Vigo <brian.vigo06@gmail.com>
2025-04-02 19:24:52 -0300

    Update L4E02.py

--- L4-19.03.2025/L4E02.py
+++ L4-19.03.2025/L4E02.py
@@ -4,14 +4,10 @@ x = int(input('Digite um valor:'))
 y = int(input('Digite outro valor:'))
 if x > y:
     y = y + 5
-    if x < y:
-        print('Este é o maior valor! Sendo ele:', y)
-    else:
-        print('Este é o maior valor! Sendo ele:', x)
-
 else:
     x = x + 5
-    if x < y:
-        print('Este é o maior valor! Sendo ele:', y)
-    else:
-        print('Este é o maior valor! Sendo ele:', x)
+
+if x > y:
+    print('Este é o maior valor! Sendo ele:', x)
+else:
+    print('Este é o maior valor! Sendo ele:', y)
